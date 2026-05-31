e96afcf2c549234b7796a720cf3b69999985eb7b
alanjaime191-hub <alanjaime191@gmail.com>
2026-05-31 15:08:04 -0300

    Update Utils.py

--- files/Utils.py
+++ files/Utils.py
@@ -1,5 +1,12 @@
 import random
 from files.Items import items
+from files import Zonas
+
+ORDEN_CATEGORIAS = [
+    "Material",
+    "Comida",
+    "Herramienta"
+]
 
 def explorar(zona):
     drop_id = random.choice(zona["drops"])
@@ -8,17 +15,42 @@ def explorar(zona):
 
     return drop_id
 
+
 def mostrar_inv(usuario):
+
+    categorias = {}
 
     for clave, cantidad in usuario["inventario"].items():
 
         item_id, calidad = clave.rsplit("_", 1)
 
-        nombre = items[item_id]["nombre"]
+        categoria = items[item_id]["tipo"]
 
-        print(
-            f"{nombre} (calidad {calidad}) x{cantidad}"
+        if categoria not in categorias:
+            categorias[categoria] = []
+
+        categorias[categoria].append(
+            (item_id, calidad, cantidad)
         )
+
+    for categoria in categorias:
+
+        print(f"\n=== {categoria} ===")
+
+        lista = sorted(
+        categorias[categoria],
+        key=lambda x: (
+        int(x[1]),
+        items[x[0]]["nombre"]
+        ))
+
+        for item_id, calidad, cantidad in lista:
+
+            print(
+                f"{items[item_id]['nombre']} "
+                f"(calidad {calidad}) "
+                f"x{cantidad}"
+            )
 
 def agregar_item(usuario, item_id, calidad, cantidad=1):
     clave = f"{item_id}_{calidad}"
@@ -27,3 +59,33 @@ def agregar_item(usuario, item_id, calidad, cantidad=1):
         usuario["inventario"][clave] = 0
 
     usuario["inventario"][clave] += cantidad
+
+def menu():
+    
+    opciones = list(Zonas.zonas.keys())
+
+    for i, zona_id in enumerate(opciones, start=1):
+
+        print(f"{i}) {Zonas.zonas[zona_id]['nombre']}")
+
+    print(f"{len(opciones)+1}) Ver inventario")
+    print(f"{len(opciones)+2}) Crafteo")
+    try:
+        eleccion = int(input("Opción: "))
+
+        if 1 <= eleccion <= len(opciones):
+            return ("zona", opciones[eleccion-1])
+
+        elif eleccion == len(opciones)+1:
+            return ("inventario", None)
+
+        elif eleccion == len(opciones)+2:
+            return ("crafteo", None)
+
+        return ("error", None)
+    
+    except: 
+        print("Donde eso fue una opcion???? panflin")
+        input("(enter para re lanzar el menu)\n")
+        return(None, None)
+    
