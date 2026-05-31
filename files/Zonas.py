9cdcfff2fcac1bde49a8b5edefa5f076ea7cf434
alanjaime191-hub <alanjaime191@gmail.com>
2026-05-31 15:07:35 -0300

    Update Zonas.py

--- files/Zonas.py
+++ files/Zonas.py
@@ -1,13 +1,18 @@
 #files.Zonas
-bosque = {
+zonas = {
     "bosque1": {
-        "nombre": "bosque1",
+        "nombre": "Bosques de palermo",
         "drops": ["madera1",
                   ]
     },
     "bosque2": {
-        "nombre": "bosque2",
+        "nombre": "El parque",
         "drops": ["madera2"
                   ]
     },
+    "mina1": {
+        "nombre": "Mina rocas soleadas",
+        "drops": ["piedra1"
+                  ]
+    }
 }
